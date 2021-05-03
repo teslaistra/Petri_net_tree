b66eba212df559214b76fb1571c83bc090dfaeec
Daniil Yefimov <danyefimoff@gmail.com>
2021-05-03 13:02:12 +0300

    expected marking calculation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import collections
+import queue
 
 number_of_points = 0
 points = dict()
@@ -9,19 +10,19 @@ edges_number = 0
 edges_transitions_to_points = collections.defaultdict(list)
 reverse_points_to_transitions = collections.defaultdict(list)
 
-global_id = 0
-markers = []
+root = None
 
 
 class Node:
-    def __init__(self):
-        self.parent = None
-        self.type_node = 'border'
-        self.children = []
-        self.mark = []
-
-
-root = Node()
+    def __init__(self, parent=None, type_node='border', children=None, mark=None):
+        if mark is None:
+            mark = []
+        if children is None:
+            children = []
+        self.parent = parent
+        self.type_node = type_node
+        self.children = children
+        self.mark = mark
 
 
 # проверка является ли дублирующей
@@ -30,10 +31,13 @@ def check_if_exist(root, Node):
 
 
 # проверка является ли терминальной
-def check_if_terminates(root, Node):
+def check_if_terminates(mark):
+    if get_available_transition(mark) is []:
+        return True
     return False
 
 
+# получение переходов, доступных для запуска
 def get_available_transition(marking):
     available = []
     for transition in reverse_points_to_transitions:
@@ -48,6 +52,7 @@ def get_available_transition(marking):
     return available
 
 
+# считывание входных данных
 def parse(file):
     global number_of_points
     global points
@@ -55,7 +60,6 @@ def parse(file):
     global transitions
     global edges_number
     global edges_transitions_to_points
-    global edges_points_to_transitions
 
     file = open(file)
     number_of_points = int(file.readline())
@@ -77,9 +81,57 @@ def parse(file):
             edges_transitions_to_points[int(fr) - 1].append(int(to) - 1)
 
 
+# обработка вершины
+def process_mark(node):
+    if check_if_exist(root, node.mark):
+        Node.type_node = 'duplicate'
+        return
+    elif check_if_terminates(node.mark):
+        node.type_node = 'terminate'
+        return
+    else:
+        calc_children(node)
+
+
+def run_transition(transition, node):
+    expected_marking = node.mark.copy()
+    child = Node()
+
+    for i in edges_transitions_to_points[transition]:
+        expected_marking[i] += 1
+
+    for i in reverse_points_to_transitions[transition]:
+        expected_marking[i] -= 1
+    print(expected_marking)
+    
+    return child
+
+# просчет переходов
+def calc_children(node):
+    print('calc')
+    transitions_to_do = queue.Queue()
+    for to_do in get_available_transition(node.mark):
+        transitions_to_do.put(to_do)
+
+    children = []
+    while transitions_to_do.qsize() > 0:
+        to_do = transitions_to_do.get()
+        new_node = run_transition(to_do, node)
+        children.append(new_node)
+    node.children = children
+
+
 def run():
+    global root
     print("Start marking is:", list(points.values()))
-    root = Node
+    root = Node(mark=list(points.values()))
+    active_nodes = queue.Queue()
+    active_nodes.put(root)
+
+    while active_nodes.qsize() > 0:
+        node = active_nodes.get()
+        process_mark(node)
+    print(root.type_node)
 
 
 if __name__ == '__main__':
@@ -90,7 +142,7 @@ if __name__ == '__main__':
     print("transitions", transitions)
     print('edges_transitions_to_points', dict(edges_transitions_to_points))
     print('reverse_points_to_transitions', dict(reverse_points_to_transitions))
+    print("_______________________________________")
     run()
-    print(get_available_transition(list(points.values())))
 # edges to transitions = {5:[1,3]}
 # edges to points = {1:[5,5]}
