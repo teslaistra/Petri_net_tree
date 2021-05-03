d1b0ac4892c01387a2eb0f5e251163341ff264dc
Daniil Yefimov <danyefimoff@gmail.com>
2021-05-03 11:44:11 +0300

    get transitions

--- main.py
+++ main.py
@@ -34,6 +34,20 @@ def check_if_terminates(root, Node):
     return False
 
 
+def get_available_transition(marking):
+    available = []
+    for transition in reverse_points_to_transitions:
+        points_from = reverse_points_to_transitions[transition]
+        is_available = True
+        for point in points_from:
+            if not marking[point]:
+                is_available = False
+        if is_available:
+            available.append(transition)
+
+    return available
+
+
 def parse(file):
     global number_of_points
     global points
@@ -47,20 +61,20 @@ def parse(file):
     number_of_points = int(file.readline())
     for i in range(0, number_of_points):
         point, mark = file.readline().split(' ')
-        points[int(point)] = int(mark)
+        points[int(point) - 1] = int(mark)
 
     number_of_transition = int(file.readline())
     for i in range(0, number_of_transition):
-        transitions.append(int(file.readline()))
+        transitions.append(int(file.readline()) - 1)
 
     edges_number = int(file.readline())
     for i in range(0, edges_number):
         fr, to = file.readline().split(' ')
 
-        if int(to) in transitions:
-            reverse_points_to_transitions[int(to)].append(int(fr))
-        if int(to) in points:
-            edges_transitions_to_points[int(fr)].append(int(to))
+        if int(to) - 1 in transitions:
+            reverse_points_to_transitions[int(to) - 1].append(int(fr) - 1)
+        if int(to) - 1 in points:
+            edges_transitions_to_points[int(fr) - 1].append(int(to) - 1)
 
 
 def run():
@@ -77,6 +91,6 @@ if __name__ == '__main__':
     print('edges_transitions_to_points', dict(edges_transitions_to_points))
     print('reverse_points_to_transitions', dict(reverse_points_to_transitions))
     run()
-
+    print(get_available_transition(list(points.values())))
 # edges to transitions = {5:[1,3]}
 # edges to points = {1:[5,5]}
